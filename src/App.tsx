3a23c5d0bf566b630d64fd00ec169a1035cacbf7
thevijaydeore <iamvijaydeore@gmail.com>
2026-02-01 11:43:13 +0530

    feat: Introduce the core `App` component, establishing global providers and defining application routes.

--- src/App.tsx
+++ src/App.tsx
@@ -45,7 +45,12 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter
+        future={{
+          v7_startTransition: true,
+          v7_relativeSplatPath: true,
+        }}
+      >
         <AuthProvider>
           <Routes>
             {/* Public Routes */}
